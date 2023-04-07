7960e40a38f78a94c8ceb10820b4083fce48a669
rashmirathnayake <129378251+rashmirathnayake@users.noreply.github.com>
2023-04-07 15:33:29 +0530

    arraystack
    sd

---
+++ stack_array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void push(int val) {
+   if(top>=n-1)
+   cout<<"Stack Overflow"<<endl;
+   else {
+      top++;
+      stack[top]=val;
+   }
+}
+void pop() {
+   if(top<=-1)
+   cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< stack[top] <<endl;
+      top--;
+   }
+}
+void display() {
+   if(top>=0) {
+      cout<<"Stack elements are:";
+      for(int i=top; i>=0; i--)
+      cout<<stack[i]<<" ";
+      cout<<endl;
+   } else
+   cout<<"Stack is empty";
+}
+
+int main{
+
+   
+    int size;
+    cout<<"enter size of stack"<<endl;
+    cin>>size;
+    pop();
+
+    return 0;
+}
